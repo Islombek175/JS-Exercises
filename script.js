03bde4189e8378b23ca31a29bd853ddb81880dcc
Islombek Matkarimov <matkaimovislambek@gmail.com>
2025-05-08 22:38:01 +0500

    16 masala ishlandi

--- script.js
+++ script.js
@@ -83,16 +83,16 @@
 
 // 10.
 // function narxniAytish() {
-// 	const ovqat1 = document.getElementById("ovqat1").value;
+// 	const ovqat = document.getElementById("ovqat").value;
 // 	let natija = "";
 // 	let somsaNarxi = 3000;
 // 	let palovNarxi = 15000;
 // 	let gummaNarxi = 2000;
-// 	if(ovqat1 === "somsa") {
+// 	if(ovqat === "somsa") {
 // 		natija = `${somsaNarxi} So'm`
-// 	} else if(ovqat1 === "palov") {
+// 	} else if(ovqat === "palov") {
 // 		natija = `${palovNarxi} So'm`
-// 	} else if(ovqat1 === "gumma") {
+// 	} else if(ovqat === "gumma") {
 // 		natija =`${somsaNarxi} So'm`
 // 	} else {
 // 		natija = "Iltimos, to'g'ri ma'lumot kiriting."
@@ -203,53 +203,111 @@
 // }
 
 // 14.
-function tekshirish() {
-	const javob1 = document.getElementById("topishmoq1").value;
-	const javob2 = document.getElementById("topishmoq2").value;
-	const javob3 = document.getElementById("topishmoq3").value;
-	const javob4 = document.getElementById("topishmoq4").value;
-	const javob5 = document.getElementById("topishmoq5").value;
-	let natija = 0;
+// function tekshirish() {
+// 	const javob1 = document.getElementById("topishmoq1").value;
+// 	const javob2 = document.getElementById("topishmoq2").value;
+// 	const javob3 = document.getElementById("topishmoq3").value;
+// 	const javob4 = document.getElementById("topishmoq4").value;
+// 	const javob5 = document.getElementById("topishmoq5").value;
+// 	let natija = 0;
 
-	if (javob1 == "1") {
-		natija += 0 
-	} else if (javob1 == "2") {
-		natija += 1
-	} else {
-		natija += 0
-	}
+// 	if (javob1 == "1") {
+// 		natija += 0 
+// 	} else if (javob1 == "2") {
+// 		natija += 1
+// 	} else {
+// 		natija += 0
+// 	}
 
-	if (javob2 == "1") {
-		natija += 1 
-	} else if (javob2 == "2") {
-		natija += 0
-	} else {
-		natija += 0
-	}
+// 	if (javob2 == "1") {
+// 		natija += 1 
+// 	} else if (javob2 == "2") {
+// 		natija += 0
+// 	} else {
+// 		natija += 0
+// 	}
 
-	if (javob3 == "1") {
-		natija += 0 
-	} else if (javob3 == "2") {
-		natija += 0
-	} else {
-		natija += 1
-	}
+// 	if (javob3 == "1") {
+// 		natija += 0 
+// 	} else if (javob3 == "2") {
+// 		natija += 0
+// 	} else {
+// 		natija += 1
+// 	}
 
-	if (javob4 == "1") {
-		natija += 0 
-	} else if (javob4 == "2") {
-		natija += 0
-	} else {
-		natija += 1
-	}
+// 	if (javob4 == "1") {
+// 		natija += 0 
+// 	} else if (javob4 == "2") {
+// 		natija += 0
+// 	} else {
+// 		natija += 1
+// 	}
 
-	if (javob5 == "1") {
-		natija += 0 
-	} else if (javob5 == "2") {
-		natija += 0
-	} else {
-		natija += 1
-	}
+// 	if (javob5 == "1") {
+// 		natija += 0 
+// 	} else if (javob5 == "2") {
+// 		natija += 0
+// 	} else {
+// 		natija += 1
+// 	}
 
-document.getElementById("natija").innerText = `5/${natija} ball`
-}
+// document.getElementById("natija").innerText = `5/${natija} ball`
+// }
+
+// 15.
+// function sotibOlish() {
+// 	const miqdor = (document.getElementById("miqdor").value);
+// 	const mahsulot = document.getElementById("mahsulot").value;
+// 	let natija = "";
+	
+// 	if (isNaN(miqdor) || miqdor <= 0) {
+// 		natija = "Iltimos, to'g'ri ma'lumot kiriting."
+// 	} else if (mahsulot == "somsa" && miqdor >= 3000) {
+// 		natija = "Sotib oldingiz."
+// 	} else if (mahsulot == "palov" && miqdor >= 15000) {
+// 		natija = "Sotib oldingiz."
+// 	} else if (mahsulot == "sarimsoq" && miqdor >= 12000) {
+// 		natija = "Sotib oldingiz."
+// 	} else {
+// 		natija = "Uzr, pul yetmadi"
+// 	}
+// 	document.getElementById("natija").innerText = natija
+// }
+
+// 16.
+// function sotibOlish() {
+// 	const checkboxes = document.querySelectorAll("input[type = 'checkbox']:not(#qariya)");
+// 	const qariya = document.getElementById("qariya").checked;
+// 	const pul = parseFloat(document.getElementById("pul").value);
+// 	let jami = 0;
+// 	let tanlangan = 0;
+// 	let natija = 0;
+
+// 	checkboxes.forEach(chk =>{
+// 		if (chk.checked) {
+// 			tanlangan ++;
+// 			jami += parseFloat(chk.value);
+// 		}
+// 	});
+
+// 	if (qariya) {
+// 		jami = jami * 0.9;
+// 	}
+
+// 	if (tanlangan >= 5) {
+// 		jami = jami * 0.85
+// 	}
+
+// 	jami = Math.round(jami)
+
+// 	if (pul >= jami) {
+// 		natija = `Siz sotib oldingiz. Jami ${jami} So'm bo'ldi. Qolgan pul ${pul - jami} So'm.`
+// 	} else {
+// 		natija = `Afsuzki, pul yetarli emas. Sizga ${jami - pul} So'm yetmadi.`
+// 	}
+
+
+// 	document.getElementById("natija").innerText = natija
+// }
+
+// 17.
